98526969b1e2f83c72f42e0fe9f85068e10e832a
khekrn <kishorenayark@gmail.com>
2023-06-15 13:41:19 +0530

    Initial Commit

---
+++ lib/funotp.ex
@@ -0,0 +1,18 @@
+defmodule Funotp do
+  @moduledoc """
+  Documentation for `Funotp`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Funotp.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/funotp_test.exs
@@ -0,0 +1,8 @@
+defmodule FunotpTest do
+  use ExUnit.Case
+  doctest Funotp
+
+  test "greets the world" do
+    assert Funotp.hello() == :world
+  end
+end
